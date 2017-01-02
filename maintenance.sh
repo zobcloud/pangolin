b86088a5d31aacb10b68735be0b6f392d18a97d0
zobcloud <zobcloud@gmail.com>
2017-01-02 00:07:42 -0500

    Update maintenance.sh

--- maintenance.sh
+++ maintenance.sh
@@ -4,8 +4,8 @@
 # Written by:      Michael H. Roberts
 # Creation Date:   Wed 29 Apr 2015 @ 16:43:00
 ################################################
-echo "ohms676@feeble" | sudo -S  apt-get -y update
-echo "ohms676@feeble" | sudo -S apt-get  -y upgrade
-echo "ohms676@feeble" | sudo -S apt-get  -y dist-upgrade
-echo "ohms676@feeble" | sudo -S apt-get -y autoremove
-echo "ohms676@feeble" | sudo -S apt-get -y autoclean
+echo "PASSWORD" | sudo -S apt -y update
+echo "PASSWORD" | sudo -S apt -y upgrade
+echo "PASSWORD" | sudo -S apt -y dist-upgrade
+echo "PASSWORD" | sudo -S apt -y autoremove
+echo "PASSWORD" | sudo -S apt -y autoclean
